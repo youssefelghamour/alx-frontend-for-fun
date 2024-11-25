e8d263ef1ca85163b38e4f0b327effc7305ddfb7
youssefelghamour <youssefelghamour@gmail.com>
2024-11-25 15:59:05 +0100

    0. Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -9,16 +9,16 @@ def main():
     
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        exit(1)
+        sys.exit(1)
     else:
         markdown_file_name = sys.argv[1]
         output_file_name = sys.argv[2]
 
         if not os.path.exists(markdown_file_name):
             sys.stderr.write("Missing {}\n".format(markdown_file_name))
-            exit(1)
+            sys.exit(1)
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
